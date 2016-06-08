1be3eb9a611ec20750ce541533de205e64ae87e3
Cruz Gerardo Yañez Teran <cruzgerardoyanezteran@Cruzs-MacBook-Pro.local>
2016-06-08 15:12:55 -0500

    First commit

---
+++ app/src/main/java/com/github/gerardo5120/tiemporetiro/MainActivity.java
@@ -0,0 +1,62 @@
+package com.github.gerardo5120.tiemporetiro;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.github.gerardo5120.zpiechart.ValueDial;
+import com.github.gerardo5120.zpiechart.ZPieChart;
+
+public class MainActivity extends AppCompatActivity {
+    private OnDrawExtDerechosHandler drawExtDerechosHandler;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ZPieChart pieChart = new ZPieChart(this,
+                new float[] { 32f, 15f, 9.25f, 3.75f });
+
+        ValueDial ageDial = new ValueDial(47f, 0f);
+
+        ValueDial retirementDial = new ValueDial(13f, 47f);
+        retirementDial.setColor(0xFFEC407A);
+
+        pieChart.getValueDials().add(ageDial);
+        pieChart.getValueDials().add(retirementDial);
+
+
+        drawExtDerechosHandler = new OnDrawExtDerechosHandler();
+        pieChart.setOnDrawChartHandler(drawExtDerechosHandler);
+        setContentView(pieChart);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_about_contacts, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                Intent intent = new Intent(this, About.class);
+
+                Bundle b = new Bundle();
+
+                intent.putExtra("Message", "Holla");
+
+                startActivity(intent);
+
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/github/gerardo5120/tiemporetiro/OnDrawExtDerechosHandler.java
@@ -0,0 +1,73 @@
+package com.github.gerardo5120.tiemporetiro;
+
+import com.github.gerardo5120.zpiechart.OnDrawChartParams;
+import com.github.gerardo5120.zpiechart.OnDrawChartSimpleHandler;
+import com.github.gerardo5120.zpiechart.OnDrawSliceParams;
+import com.github.gerardo5120.zpiechart.OnDrawValueDialParams;
+
+import java.text.SimpleDateFormat;
+import java.util.GregorianCalendar;
+
+/**
+ * Created by cruzgerardoyanezteran on 27/5/16.
+ */
+public class OnDrawExtDerechosHandler extends OnDrawChartSimpleHandler {
+    private GregorianCalendar birthDate;
+    private GregorianCalendar now;
+    private SimpleDateFormat dateFormat;
+
+    private GregorianCalendar weeksAgo;
+
+    public OnDrawExtDerechosHandler() {
+        birthDate = new GregorianCalendar(1988, 02, 15);
+        dateFormat = new SimpleDateFormat("dd-MM-yyyy");
+        weeksAgo = new GregorianCalendar();
+
+        // System.out.println(dateFormat.format(birthDate.getTime()));
+    }
+
+    @Override
+    public boolean onDrawBegin(OnDrawChartParams params) {
+        weeksAgo.set(birthDate.get(GregorianCalendar.YEAR),
+                birthDate.get(GregorianCalendar.MONTH),
+                birthDate.get(GregorianCalendar.DAY_OF_MONTH));
+
+        return true;
+    }
+
+    @Override
+    public boolean onDrawValueDial(OnDrawChartParams params) {
+        OnDrawValueDialParams dialParams = (OnDrawValueDialParams) params;
+
+        if (dialParams.getValue() == 47)
+            dialParams.setText("Tienes " + String.valueOf(dialParams.getValue()) + " años de edad.");
+        else if (dialParams.getValue() == 13)
+            dialParams.setText("Faltan " + String.valueOf(12) + " años p/retiro");
+
+        return super.onDrawValueDial(params);
+    }
+
+    @Override
+    public boolean onDrawSlice(OnDrawChartParams params) {
+        OnDrawSliceParams sliceParams = (OnDrawSliceParams) params;
+
+        if (sliceParams.getValue() == 32) {
+            sliceParams.setText("A los 32 años empezaste");
+        }
+        else if (sliceParams.getValue() == 15) {
+            sliceParams.setText("772 sem's cot's (15 años)");
+        }
+        else if (sliceParams.getValue() == 9.25) {
+            sliceParams.setText(sliceParams.getValue() + " años");
+        }
+
+        else if (sliceParams.getValue() == 3.75) {
+            sliceParams.setText("Ext. derechos: " + sliceParams.getValue() + " años");
+        }
+        else if (sliceParams.getValue() == 1.0f)
+            sliceParams.setText("Hello ;)");
+
+
+        return super.onDrawSlice(params);
+    }
+}
